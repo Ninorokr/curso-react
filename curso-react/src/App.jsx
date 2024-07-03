b759368bbb1e2339788c400f3fa6b7f477db12c5
AlvaroG <ninorokr2@gmail.com>
2024-07-02 21:49:14 -0500

    clase 3 otra vez

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import SugarSyntax from './clase3/SugarSyntax'
 import Componente3 from './clase3/Componente3'
+import Props from './clase4/Props'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Componente3 />
+      <Props academia={"Coderhouse"} curso={"React JS"} logo={"https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"}/>
     </>
   )
 }
