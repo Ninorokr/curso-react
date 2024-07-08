77cbf59d1e4a8bd7a78f95df2189f80ca7401dc7
AlvaroG <ninorokr2@gmail.com>
2024-07-07 23:52:05 -0500

    avance

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import SugarSyntax from './clase3/SugarSyntax'
 import Componente3 from './clase3/Componente3'
 import Props from './clase4/Props'
+import States from './clase4/States'
+import CompPresentacion from './clase4/CompPresentacion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Props academia={"Coderhouse"} curso={"React JS"} logo={"https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"}/>
+      {/* <Props academia={"Coderhouse"} curso={"React JS"} logo={"https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"}/> */}
+      {/* <States /> */}
+      <CompPresentacion titulo1={"Coderhouse"} titulo2={"Curso de React JS"}/>
     </>
   )
 }
--- curso-react/src/clase4/Props.jsx
+++ curso-react/src/clase4/Props.jsx
@@ -1,5 +1,7 @@
 //Ejemplo #1 Recibo una prop como objeto
 
+import States from "./States";
+
 /*const Props = (prop) => {
     return (
         <>
@@ -13,29 +15,25 @@ export default Props*/
 
 //Los estados se definen siempre DENTRO de un componente
 
-const Logo = ({url, texto, ancho}) => {
-    return (
-        <img src={url} alt={texto} width={ancho} className="img-thumbnail" />
-    )
-}
+const Logo = ({ url, texto, ancho }) => {
+  return <img src={url} alt={texto} width={ancho} className="img-thumbnail" />;
+};
 
 //Ejemplo #2 => Recibo una prop y la desestructuro (destructuring)
-const Props = ({academia, logo, curso}) => {
 
-    let ubicacion = "Lima, Perú"
+const Props = ({ academia, logo, curso }) => {
+  let ubicacion = "Lima, Perú";
 
-    return (
-        <div className="container my-5">
-            <div className="row">
-                <div className="col text-center">
-                    <h1 className="display-1">{academia}</h1>
-                    <h3 className="display-3">{curso + " (Carrera FrontEnd)"}</h3>
-                    <p><Logo ancho={240} texto={curso} url={logo} /></p>
-                    <p>{ubicacion}</p>
-                </div>
-            </div>
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col text-center">
+          <h1 className="display-1">{academia}</h1>
+          <h3 className="display-3">{curso + " (Carrera Frontend)"}</h3>
+          <p><Logo ancho={240} texto={curso} url={logo}/></p>
         </div>
-    )
-}
-
-export default Props
+      </div>
+    </div>
+  );
+};
+export default Props;
