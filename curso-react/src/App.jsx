224ea6de759e7eb28a27b16e01482ba6997418aa
AlvaroG <ninorokr2@gmail.com>
2024-07-08 20:31:24 -0500

    x

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -4,6 +4,7 @@ import Componente3 from './clase3/Componente3'
 import Props from './clase4/Props'
 import States from './clase4/States'
 import CompPresentacion from './clase4/CompPresentacion'
+import CompContenedor from './clase4/CompContenedor'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
     <>
       {/* <Props academia={"Coderhouse"} curso={"React JS"} logo={"https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"}/> */}
       {/* <States /> */}
-      <CompPresentacion titulo1={"Coderhouse"} titulo2={"Curso de React JS"}/>
+      {/* <CompPresentacion titulo1={"Coderhouse"} titulo2={"Curso de React JS"}/> */}
+      <CompContenedor />
     </>
   )
 }
